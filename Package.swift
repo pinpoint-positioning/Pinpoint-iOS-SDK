f343e201cdd82893cbfbd56609835d61e2baf862
schech1 <76683319+schech1@users.noreply.github.com>
2025-09-25 10:02:08 +0200

    Fixed path

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PinpointSDK",
-            url: "https://github.com/pinpoint-positioning/EasylocateSDK/releases/download/12.1.0/PinpointSDK.xcframework.zip",
+            url: "https://github.com/pinpoint-positioning/Pinpoint-iOS-SDK/releases/download/12.1.0/PinpointSDK.xcframework.zip",
             checksum: "02272a94d2f5bc69b0c5e1e7b200c1ae6bf78cfd0465343333b19baaada3d662"
         ),
     ]
