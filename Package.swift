5a71eac5f7c928e7947c8bb62f89ad6cc2c4f1fe
schech1 <76683319+schech1@users.noreply.github.com>
2025-09-25 09:56:41 +0200

    update checksum and name

--- Package.swift
+++ Package.swift
@@ -2,21 +2,21 @@
 import PackageDescription
 
 let package = Package(
-    name: "EasylocateSDK",
+    name: "PinpointSDK",
     platforms: [
         .iOS(.v17)
     ],
     products: [
         .library(
-            name: "EasylocateSDK",
-            targets: ["EasylocateSDK"]
+            name: "PinpointSDK",
+            targets: ["PinpointSDK"]
         ),
     ],
     targets: [
         .binaryTarget(
-            name: "EasylocateSDK",
-            url: "https://github.com/pinpoint-positioning/EasylocateSDK/releases/download/12.1.0/EasylocateSDK.xcframework.zip",
-            checksum: "8f024f8801dfdf3abd67a85a5932a3544cd0eeede3c09a4b26084bdc9e59142f"
+            name: "PinpointSDK",
+            url: "https://github.com/pinpoint-positioning/EasylocateSDK/releases/download/12.1.0/PinpointSDK.xcframework.zip",
+            checksum: "02272a94d2f5bc69b0c5e1e7b200c1ae6bf78cfd0465343333b19baaada3d662"
         ),
     ]
 )
