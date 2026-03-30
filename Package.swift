e0b2790ffe0a6e9e2c6bfddce5096177a91f92d1
schech1 <76683319+schech1@users.noreply.github.com>
2026-03-30 21:53:53 +0200

    swift package fix

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "PinpointSDK",
-            url: "https://github.com/pinpoint-positioning/Pinpoint-iOS-SDK/releases/download/12.2.2/PinpointSDK.xcframework.zip",
+            url: "https://github.com/pinpoint-positioning/Pinpoint-iOS-SDK/releases/download/12.2.2/PinpointSDKFramework.zip",
             checksum: "2429c6fcebad539e0ad402cc6e0f88a752890cef57a86944811648d9ac1b71cb"
         ),
     ]
